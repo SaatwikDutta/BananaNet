aff457350d815932564bc1f2f6c715260f58fe21
SAATWIK DUTTA <88401922+SaatwikDutta@users.noreply.github.com>
2023-12-02 07:40:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,12 +55,11 @@ st.title('Hello there!')
 
 hide_st_style = """
             <style>
-            
-            
-           
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
-
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
